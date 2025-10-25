2c3db4e44c66d3ffb0c3e14cfc773b96edb51135
greydoubt <43443470+greydoubt@users.noreply.github.com>
2025-10-25 00:59:07 -0700

    Create cuda_streams_03_kernal_overlap.cu

---
+++ cuda_streams_03_kernal_overlap.cu
@@ -0,0 +1,173 @@
+//Using Streams to Overlap Data Transfers and Code Execution
+// In addition to cudaMemcpy is cudaMemcpyAsync which can asynchronously copy memory either from host to device or from device to host as long as the host memory is pinned, which can be done by allocating it with cudaMallocHost.
+//
+
+/*
+int N = 2<<24;
+int size = N * sizeof(int);
+
+int *host_array;
+int *device_array;
+
+cudaMallocHost(&host_array, size);               // Pinned host memory allocation.
+cudaMalloc(&device_array, size);                 // Allocation directly on the active GPU device.
+
+initializeData(host_array, N);                   // Assume this application needs to initialize on the host.
+
+const int numberOfSegments = 4;                  // This example demonstrates slicing the work into 4 segments.
+int segmentN = N / numberOfSegments;             // A value for a segment's worth of `N` is needed.
+size_t segmentSize = size / numberOfSegments;    // A value for a segment's worth of `size` is needed.
+
+// For each of the 4 segments...
+for (int i = 0; i < numberOfSegments; ++i)
+{
+  // Calculate the index where this particular segment should operate within the larger arrays.
+  segmentOffset = i * segmentN;
+
+  // Create a stream for this segment's worth of copy and work.
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+  
+  // Asynchronously copy segment's worth of pinned host memory to device over non-default stream.
+  cudaMemcpyAsync(&device_array[segmentOffset],  // Take care to access correct location in array.
+                  &host_array[segmentOffset],    // Take care to access correct location in array.
+                  segmentSize,                   // Only copy a segment's worth of memory.
+                  cudaMemcpyHostToDevice,
+                  stream);                       // Provide optional argument for non-default stream.
+                  
+  // Execute segment's worth of work over same non-default stream as memory copy.
+  kernel<<<number_of_blocks, threads_per_block, 0, stream>>>(&device_array[segmentOffset], segmentN);
+  
+  // `cudaStreamDestroy` will return immediately (is non-blocking), but will not actually destroy stream until
+  // all stream operations are complete.
+  cudaStreamDestroy(stream);
+}
+
+*/
+#include <stdio.h>
+
+__global__
+void initWith(float num, float *a, int N)
+{
+
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for(int i = index; i < N; i += stride)
+  {
+    a[i] = num;
+  }
+}
+
+__global__
+void addVectorsInto(float *result, float *a, float *b, int N)
+{
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for(int i = index; i < N; i += stride)
+  {
+    result[i] = a[i] + b[i];
+  }
+}
+
+void checkElementsAre(float target, float *vector, int N)
+{
+  for(int i = 0; i < N; i++)
+  {
+    if(vector[i] != target)
+    {
+      printf("FAIL: vector[%d] - %0.0f does not equal %0.0f\n", i, vector[i], target);
+      exit(1);
+    }
+  }
+  printf("Success! All values calculated correctly.\n");
+}
+
+int main()
+{
+  int deviceId;
+  int numberOfSMs;
+
+  cudaGetDevice(&deviceId);
+  cudaDeviceGetAttribute(&numberOfSMs, cudaDevAttrMultiProcessorCount, deviceId);
+
+  const int N = 2<<24;
+  size_t size = N * sizeof(float);
+
+  float *a;
+  float *b;
+  float *c;
+  float *h_c;
+
+  cudaMalloc(&a, size);
+  cudaMalloc(&b, size);
+  cudaMalloc(&c, size);
+  cudaMallocHost(&h_c, size);
+
+  size_t threadsPerBlock;
+  size_t numberOfBlocks;
+
+  threadsPerBlock = 256;
+  numberOfBlocks = 32 * numberOfSMs;
+
+  cudaError_t addVectorsErr;
+  cudaError_t asyncErrInit;
+  cudaError_t asyncErrAdd;
+
+  /*
+   * Create 3 streams to run initialize the 3 data vectors in parallel.
+   */
+
+  cudaStream_t stream1, stream2, stream3;
+  cudaStreamCreate(&stream1);
+  cudaStreamCreate(&stream2);
+  cudaStreamCreate(&stream3);
+
+  /*
+   * Give each `initWith` launch its own non-standard stream.
+   */
+
+  initWith<<<numberOfBlocks, threadsPerBlock, 0, stream1>>>(3, a, N);
+  initWith<<<numberOfBlocks, threadsPerBlock, 0, stream2>>>(4, b, N);
+  initWith<<<numberOfBlocks, threadsPerBlock, 0, stream3>>>(0, c, N);
+
+  /*
+   * Synchronize - we are using different non-standard streams for initialisation
+   * and calcs, so we need to explicitly synchronize to ensure all data has been
+   * initialised before starting the calculations 
+   */
+  asyncErrInit = cudaDeviceSynchronize();
+  if(asyncErrInit != cudaSuccess) printf("Error init: %s\n", cudaGetErrorString(asyncErrInit));
+
+  for (int i = 0; i < 4; ++i)
+  {
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+
+    addVectorsInto<<<numberOfBlocks/4, threadsPerBlock, 0, stream>>>(&c[i*N/4], &a[i*N/4], &b[i*N/4], N/4);
+    cudaMemcpyAsync(&h_c[i*N/4], &c[i*N/4], size/4, cudaMemcpyDeviceToHost, stream);
+    cudaStreamDestroy(stream);
+  }
+
+  addVectorsErr = cudaGetLastError();
+  if(addVectorsErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(addVectorsErr));
+
+  asyncErrAdd = cudaDeviceSynchronize();
+  if(asyncErrAdd != cudaSuccess) printf("Error add: %s\n", cudaGetErrorString(asyncErrAdd));
+
+  checkElementsAre(7, h_c, N);
+
+  /*
+   * Destroy streams when they are no longer needed.
+   */
+
+  cudaStreamDestroy(stream1);
+  cudaStreamDestroy(stream2);
+  cudaStreamDestroy(stream3);
+
+  cudaFree(a);
+  cudaFree(b);
+  cudaFree(c);
+  cudaFreeHost(h_c);
+}
